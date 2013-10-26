70f983a158e109401430e4516479fb9fa4b00670
Decio Battaglia <decio.battaglia@gmail.com>
2013-10-26 17:40:55 +0100

    formatting

--- app/main.js
+++ app/main.js
@@ -1,10 +1,10 @@
 define({
 
-	// Load a basic theme. This is just a CSS file, and since a moduleLoader is
-	// configured in run.js, curl knows to load this as CSS.
-	theme: { module: 'theme/basic.css' },
+  // Load a basic theme. This is just a CSS file, and since a moduleLoader is
+  // configured in run.js, curl knows to load this as CSS.
+  theme: { module: 'theme/basic.css' },
 
-	default_language: "italian",
+  default_language: "italian",
   inactive_class: "inactive",
 
   get_siblings: { module: "app/utils/get_siblings"},
@@ -47,8 +47,8 @@ define({
     }
   },
 
-	// Wire.js plugins
-	plugins: [
+  // Wire.js plugins
+  plugins: [
     { module: 'wire/dom', classes: { init: 'loading' } },
     { module: 'wire/dom/render' },
     { module: 'wire/on' },
